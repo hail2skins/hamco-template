3c59233e4548011e2d4b6296b4fbafd9ea31f332
Art Mills <art@hamcois.com>
2023-04-03 20:00:00 -0500

    Very basic auth is now active

--- src/web/main.go
+++ src/web/main.go
@@ -14,7 +14,24 @@ import (
 func main() {
 	loadEnv()
 	loadDatabase()
+	serveApplication()
+}
 
+func loadDatabase() {
+	database.Connect()
+	database.Database.AutoMigrate(&model.User{})
+	database.Database.AutoMigrate(&model.Post{})
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func serveApplication() {
+	//r := gin.Default()
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
@@ -29,19 +46,13 @@ func main() {
 	r.LoadHTMLGlob("templates/**/*")
 	controller.Router(r)
 
+	publicRoutes := r.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
 	log.Println("Server started")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
 
-func loadDatabase() {
-	database.Connect()
-	database.Database.AutoMigrate(&model.User{})
-	database.Database.AutoMigrate(&model.Post{})
-}
-
-func loadEnv() {
-	err := godotenv.Load(".env.local")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	//r.Run(":8000")
+	//fmt.Println("Server running on port 8000")
 }
